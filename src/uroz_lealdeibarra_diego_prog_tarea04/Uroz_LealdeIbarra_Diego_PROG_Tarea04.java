75f1c5c63ee3da8d08ee0ba9c3b181a924310b84
pukup <urozdiego@gmail.com>
2017-11-19 21:03:25 +0100

    Estructura de archivos NetBeans + "utilidades"

---
+++ src/uroz_lealdeibarra_diego_prog_tarea04/Uroz_LealdeIbarra_Diego_PROG_Tarea04.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uroz_lealdeibarra_diego_prog_tarea04;
+
+/**
+ *
+ * @author pukup
+ */
+public class Uroz_LealdeIbarra_Diego_PROG_Tarea04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
