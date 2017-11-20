703aceb6309c0e62e0543e5e26f8f2a2f7c956a9
pukup <urozdiego@gmail.com>
2017-11-20 19:35:59 +0100

    Clase "Principal + Triangulo[]

--- src/arrays/Principal.java
+++ src/arrays/Principal.java
@@ -24,6 +24,18 @@ public class Principal {
         
         Punto[] vectorPuntos = new Punto[numeroTriangulos*3];
         
+        for (int i = 0; i < vectorPuntos.length; i++) {
+            
+            vectorPuntos[i] = new Punto((double)(Math.random()*10),(double)(Math.random()*10));
+        }
+        
+        Triangulo[] vectorTriangulos = new Triangulo[numeroTriangulos];
+        
+        for (int i = 0; i < numeroTriangulos; i+=3) {
+            vectorTriangulos[i] = new Triangulo (vectorPuntos[i],vectorPuntos[i+1],vectorPuntos[i+2]);
+        }
+
+            
         
     }
     
