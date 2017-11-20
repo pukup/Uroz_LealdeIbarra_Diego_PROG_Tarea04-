2af6f1bf2d3c604ea8433608030f4796613f3b72
pukup <urozdiego@gmail.com>
2017-11-20 17:57:24 +0100

    clase "Punto" + getters + "toString"

--- src/arrays/Punto.java
+++ src/arrays/Punto.java
@@ -17,8 +17,22 @@ public class Punto {
         this.x = x;
         this.y = y;
     }
+
+
+    public double getX() {
+        return x;
+    }
+
+
+    public double getY() {
+        return y;
+    }
     
-    
+
+    public String toString(){
+        String toString;
+        return toString = "(" + x + ", " + y + (")");
+    }
     
     
 }
